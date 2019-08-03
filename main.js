37c5c95973d5017e06c0f942c0257bbe541675e0
satuturn <saturningaround@gmail.com>
2019-08-03 15:02:47 +0800

    介紹變數與透過 ID 取得元素

---
+++ main.js
@@ -0,0 +1,8 @@
+// 變數 變數名稱 = 值
+var text = "我是標題";       // 字串
+var number = 7;             // 整數
+var time = 1.5;             // 浮點數
+var check = true;           // 布林值 true / false
+
+// 網頁文件.透過 ID 取得元素.插入 HTML = 值;
+document.getElementById("title").innerHTML = time;
